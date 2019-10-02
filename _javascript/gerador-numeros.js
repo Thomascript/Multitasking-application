65cffcf4be82a42dc307c3c75a760b816883eb14
Saulo Joab <37988252+saulojoab@users.noreply.github.com>
2019-10-02 07:55:12 -0300

    melhorando funcao de gerador de numeros
    ao inves de criar uma linha pra cada numero, coloquei tudo dentro de um for

--- _javascript/gerador-numeros.js
+++ _javascript/gerador-numeros.js
@@ -1,15 +1,10 @@
 //script para gerar os números aleatórios correspondentes a mega-sena da virada do fim de ano. Boa sorte!
 function gerador(){ //função responsável por gerar 6 dezenas ao pressionar o botão do evento onclick.
-    var loteria = Math.floor(Math.random() * 60); 
-	document.getElementById('1').innerHTML = loteria + 1;
-	var loteria = Math.floor(Math.random() * 60);
-	document.getElementById('2').innerHTML = loteria + 1;
-	var loteria = Math.floor(Math.random() * 60);
-	document.getElementById('3').innerHTML = loteria + 1;
-	var loteria = Math.floor(Math.random() * 60);
-	document.getElementById('4').innerHTML = loteria + 1;
-	var loteria = Math.floor(Math.random() * 60);
-	document.getElementById('5').innerHTML = loteria + 1;
-	var loteria = Math.floor(Math.random() * 60);
-	document.getElementById('6').innerHTML = loteria + 1;
+  var loteria;
+  
+  for (i = 1; i==6; i++)
+  {
+    	loteria = Math.floor(Math.random() * 60); 
+	document.getElementById(String(i)).innerHTML = loteria + 1;
+  }
 }
