aa894f48f6b4976f1f58c7b40b9a9cf3549cc8fc
renanzxc <renanzx@live.com>
2020-10-12 20:22:52 -0300

    código cálculo

--- _javascript/calculadora.js
+++ _javascript/calculadora.js
@@ -1,15 +1,16 @@
-//arquivo javascript para minha calculadora
-//criando a função de limpar a tela com o botão C.
+// arquivo javascript para minha calculadora
+// criando a função de limpar a tela com o botão C.
 function limpar(){
     document.getElementById('campoVisor').value = '';
 }
-//criando função para recolher valores na tela em sequência, em concatenação.
+// criando função para recolher valores na tela em sequência, em concatenação.
 function agrupar(valor){
     document.getElementById('campoVisor').value += valor;
 }
-//criando função para realizar os cálculos.
+// criando função para realizar os cálculos.
 function calculos(){
-    var resultado = 0;
+    let resultado = 0;
+
     resultado = document.getElementById('campoVisor').value;
     document.getElementById('campoVisor').value = '';
 
